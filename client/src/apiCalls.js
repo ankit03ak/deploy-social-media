ecc2a1ec28ec806dad1c7927f90c964e76c2c060
Ankit Kumar <vickyak1339@gmail.com>
2024-09-24 19:11:30 +0530

    fixed backend url

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -2,7 +2,7 @@ import axios from "axios"
 export const loginCall = async (userCredential, dispatch) =>{
     dispatch({type : "LOGIN_START"});
     try {
-        const res = await axios.post("auth/login", userCredential);
+        const res = await axios.post("https://deploy-social-media-ap1.onrender.com/api/auth/login", userCredential);
         dispatch({type : "LOGIN_SUCCESS", payload : res.data})
     } catch (error) {
         dispatch({type : "LOGIN_FAILURE", payload : error})
